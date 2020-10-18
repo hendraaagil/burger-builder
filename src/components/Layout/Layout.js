f162d388411d9e2b0bff3fc25bdfb23118eed4ca
hendraaagil <hendraaagil@gmail.com>
2020-10-18 15:50:51 +0700

    Reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,33 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import { Component } from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
-const Layout = (props) => (
-  <Auxillary>
-    <Toolbar />
-    <SideDrawer />
-    <main className={styles.Content}>{props.children}</main>
-  </Auxillary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  closeSideDrawer = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Auxillary>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          close={this.closeSideDrawer}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Auxillary>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,29 @@
 import React from 'react';
 
+import Auxillary from '../../../hoc/Auxillary';
 import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import NavItems from '../NavItems/NavItems';
 import styles from './SideDrawer.module.css';
 
 const SideDrawer = (props) => {
-  // ...
+  let attachedClasses = [styles.SideDrawer, styles.Close];
+  if (props.open) {
+    attachedClasses = [styles.SideDrawer, styles.Open];
+  }
 
   return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
+    <Auxillary>
+      <Backdrop show={props.open} close={props.close} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavItems />
+        </nav>
       </div>
-      <nav>
-        <NavItems />
-      </nav>
-    </div>
+    </Auxillary>
   );
 };
 
