49fa1d84a1331ac9be60b46fe5409863fbfe8b07
hendraaagil <hendraaagil@gmail.com>
2020-12-24 17:54:06 +0700

    Working on an Error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
           ],
         },
         value: '',
+        validation: {},
         valid: true,
       },
     },
@@ -118,6 +119,9 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
     let isValid = true;
+    if (!rules) {
+      return true;
+    }
 
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
